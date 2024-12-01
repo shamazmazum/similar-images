151b73dd5b7a92e48e8b7bdcf191b25302f94951
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-12-01 07:49:05 +0300

    Fix formatting

--- src/reporter.lisp
+++ src/reporter.lisp
@@ -1,8 +1,8 @@
 (in-package :similar-images)
 
 (defstruct progress-state
-  (last-reported      0    :type unsigned-byte)
-  (currently-reported 0    :type unsigned-byte)
+  (last-reported      0 :type unsigned-byte)
+  (currently-reported 0 :type unsigned-byte)
   (report-each     1000 :type unsigned-byte))
 
 (defun report-processed (state items-processed)
